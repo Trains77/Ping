9384977b9d7acc24634908eb38089a4816d9ce20
Trains77 <trains77videos123@gmail.com>
2022-04-02 11:23:00 -0400

    Added Two_balls, increase_ball_speed, and bot_player_1 options

--- Ping/main.py
+++ Ping/main.py
@@ -19,13 +19,33 @@ def image_display(surface, filename, xy):
 def newPlayer(x, y, userID):
     move_down = move_Down
     move_up = move_Up
+    move_up1 = move_Up1
+    move_down1 = move_Down1
     if userID == 0:
-        if state[pygame.K_w]:
-            if not y <= game_border2:
-                y = y - playerspeed
-        if state[pygame.K_s]:
-            if not y + square_length - square_size >= game_border1:
-                y = y + playerspeed
+        if bot_player_1 == False:
+            if state[pygame.K_w]:
+                if not y <= game_border2:
+                    y = y - playerspeed
+            if state[pygame.K_s]:
+                if not y + square_length - square_size >= game_border1:
+                    y = y + playerspeed
+        else:
+            if ball[1] - y <= 30:
+                move_down1 = False
+            elif ball[1] - y >= 80:
+                move_down1 = True
+            if y - ball[1] <= -29:
+                move_up1 = False
+            elif y - ball[1] <= -9:
+                move_up1 = True
+
+            if ball[0] <= 325:
+                if move_up1 == True:
+                    if not y <= game_border2:
+                        y -= playerspeed
+                elif move_down1 == True:
+                    if not y + square_length - square_size >= game_border1:
+                        y += playerspeed
     elif userID == 1:
         if bot_player_2 == False:
             if state[pygame.K_UP]:
@@ -51,7 +71,7 @@ def newPlayer(x, y, userID):
                 elif move_down == True:
                     if not y + square_length - square_size >= game_border1:
                         y += playerspeed
-    return x, y, move_down, move_Up
+    return x, y, move_down, move_up, move_down1, move_up1
 # Display
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption(GameName)
@@ -75,6 +95,14 @@ while not done:
 
         screen.fill((0, 0, 0))
         if game_started == True:
+            game_tick += 1
+            if game_tick == fps:
+                game_tick = 0
+                game_tick2 += 1
+                if game_tick2 >= fps / 4:
+                    game_tick2 = 0
+                    if increase_ball_speed == True:
+                        ballspeed += 1
             # Up Down Movement
             if ball[3] == "Down":
                 if ball[1] < game_border1:
@@ -100,22 +128,53 @@ while not done:
                     ball[0] += ballspeed
                 else:
                     game_started = False
-            player1[0], player1[1], move_Down, move_Up = newPlayer(player1[0],player1[1],0)
-            player2[0], player2[1], move_Down, move_Up = newPlayer(player2[0],player2[1],1)
+            if Two_balls == True:
+                if ball2[3] == "Down":
+                    if ball2[1] < game_border1:
+                        ball2[1] += ballspeed
+                    else:
+                        ball2[3] = "Up"
+                        ball2[1] -= ballspeed
+                        playsound(1, "Sounds/hit.wav")
+                elif ball2[3] == "Up":
+                    if ball2[1] > game_border2:
+                        ball2[1] -= ballspeed
+                    else:
+                         ball2[3] = "Down"
+                         ball2[1] += ballspeed
+                         playsound(1, "Sounds/hit.wav")
+                if ball2[2] == "Left":
+                    if ball2[0] > game_border2:
+                        ball2[0] -= ballspeed
+                    else:
+                        game_started = False
+                else:
+                    if ball2[0] < game_border3:
+                        ball2[0] += ballspeed
+                    else:
+                        game_started = False
+            player1[0], player1[1], move_Down, move_Up, move_Down1, move_Up1 = newPlayer(player1[0],player1[1],0)
+            player2[0], player2[1], move_Down, move_Up, move_Down1, move_Up1 = newPlayer(player2[0],player2[1],1)
         else:
+            ballspeed = ball_speed
             left = int(math.ceil(random.randint(1,2)))
             right = int(math.ceil(random.randint(1,2)))
             if left == 1:
                 Left = "Left"
+                LeftTwo = "Right"
             else:
                 Left = "Right"
+                LeftTwo = "Left"
             if right == 1:
                 Up = "Up"
+                UpTwo = "Down"
             else:
                 Up = "Down"
+                UpTwo = "Up"
             player1 = [25,250]
             player2 = [655, 250]
             ball = [350, 250, Left, Up]
+            ball2 = [350, 250, LeftTwo, UpTwo]
 
             if mouse_button_list[0] == True:
                 cursor_square = pygame.draw.rect(screen, (255,0,0), [cursor_pos[0], cursor_pos[1], square_size,square_size])
@@ -135,12 +194,22 @@ while not done:
         player1_square = pygame.draw.rect(screen, square_color, [player1[0],player1[1],square_size,square_length])
         player2_square = pygame.draw.rect(screen, square_color, [player2[0],player2[1],square_size,square_length])
         ball_square = pygame.draw.rect(screen, square_color, [ball[0],ball[1],square_size,square_size])
+        if Two_balls == True:
+            ball2_square = pygame.draw.rect(screen, square_color, [ball2[0],ball2[1],square_size,square_size])
+        else:
+            ball2_square = pygame.draw.rect(screen, square_color, [999,999,square_size,square_size])
 
         if pygame.Rect.colliderect(ball_square, player1_square) == 1:
             ball[2] = "Right"
             playsound(1, "Sounds/hit.wav")
         if pygame.Rect.colliderect(ball_square, player2_square) == 1:
             ball[2] = "Left"
+            playsound(1, "Sounds/hit.wav")
+        if pygame.Rect.colliderect(ball2_square, player1_square) == 1:
+            ball2[2] = "Right"
+            playsound(1, "Sounds/hit.wav")
+        if pygame.Rect.colliderect(ball2_square, player2_square) == 1:
+            ball2[2] = "Left"
             playsound(1, "Sounds/hit.wav")
         pygame.display.flip()
 # Quite the execution when clicking on close
--- Ping/settings.py
+++ Ping/settings.py
@@ -4,26 +4,36 @@ import pygame
 pygame.init()
 
 #Script Settings
-bot_player_2 = True # Use AI for player 2
-ballspeed = 5 # speed the ball moves
+ball_speed = 5 # speed the ball moves
 enable_audio = True # Enable audio
 playerspeed = 10 # How fast the player moves
 
+# Gamemodes
+bot_player_2 = True # Use a bot for player 2, does not work with Two_balls
+bot_player_1 = True # Use a bot for player 1, does not work with Two_balls
+increase_ball_speed = False # Increase the ball's speed over time
+Two_balls = False # Have 2 balls instead of one, does not work with bot_player_2
+
 # Internal variables
-size = [700, 500] # How big the game window should be
-GameName = "Ping" # The name of the game
-square_color = (255, 255, 255) # The color code of the player square
-square_size = 15 # How big should a square be
+size = [700, 500]
+GameName = "Ping"
+square_color = (255, 255, 255)
+square_size = 15
 square_length = 80
-game_border1 = size[1] - square_size # The maximum coordinates a player can move
-game_border2 = 0 # The minimum coordinates a player can have
+game_border1 = size[1] - square_size
+game_border2 = 0
 game_border3 = size[0] - square_size
-fps = 30 # The fps of the game
+fps = 30
 player1 = [25,250]
 player2 = [655, 250]
 game_started = False
 ball = [350, 250, "Left", "Up"]
+ball2 = [350, 250, "Left", "Up"]
 font1 = pygame.font.SysFont('A totally real font', 100)
 font2 = pygame.font.SysFont('A totally real font', 40)
 move_Down = True
 move_Up = True
+move_Down1 = True
+move_Up1 = True
+game_tick = 0
+game_tick2 = 0
